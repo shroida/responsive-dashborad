fcc97cb4f093a6594cb400b3301da1df6fecd548
shroida <shroida012@gmail.com>
2025-01-18 14:46:42 +0200

    create list for drawer items

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -13,9 +13,6 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(
           height: 8,
         ),
-        
-      
-      
       ]),
     );
   }
---
+++ lib/widgets/drawer_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dashboard/models/drawer_item_model.dart';
+import 'package:responsive_dashboard/utlis/app_styles.dart';
+
+class DrawerItem extends StatelessWidget {
+  const DrawerItem({super.key, required this.drawerItemModel});
+  final DrawerItemModel drawerItemModel;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.image),
+      title: Text(
+        drawerItemModel.title,
+        style: AppStyles.styleRegular16(context),
+      ),
+    );
+  }
+}
