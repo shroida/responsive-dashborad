921944bfceb8f6e0a94fdf988dc548eff4d33525
shroida <shroida012@gmail.com>
2025-01-18 13:14:35 +0200

    adding assets to pubspec

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
-const DashboardDesktopLayout({ super.key });
+  const DashboardDesktopLayout({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard Desktop Layout'),
+      ),
+      body: Center(
+        child: SvgPicture.asset(
+          'assets/images/avatar.svg',
+          width: 100.0,
+          height: 100.0,
+        ),
+      ),
+    );
   }
 }
