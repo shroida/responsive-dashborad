4a8bb2edd09e816a4fe2fb551195b339a4c48a79
shroida <shroida012@gmail.com>
2025-01-18 13:34:24 +0200

    add fonts to pubspec.yaml

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dashboard/utlis/app_images.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -8,13 +9,13 @@ class DashboardDesktopLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard Desktop Layout'),
+        title: const Text('Dashboard Desktop Layout'),
       ),
       body: Center(
         child: SvgPicture.asset(
-          'assets/images/avatar.svg',
-          width: 100.0,
-          height: 100.0,
+          Assets.imagesIncome,
+          width: 1000.0,
+          height: 1000.0,
         ),
       ),
     );
