7b6d5a3b54ef9f9d5303c4aed741a09f0f1cd501
shroida <shroida012@gmail.com>
2025-01-18 12:46:54 +0200

    add DashboardDesktopLayout and call it in adaptive layout

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/adaptive_layout.dart';
+import 'package:responsive_dashboard/widgets/dashboard_desktop_layout.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold ();
+    return Scaffold(
+      body: AdaptiveLayout(
+          mobileLayout: (context) => const SizedBox(),
+          tabletLayout: (context) => const SizedBox(),
+          desktopLayout: (context) => const DashboardDesktopLayout()),
+    );
   }
 }
