8941f09f051c3953133849a5f80a702a2dbc83d9
shroida <shroida012@gmail.com>
2025-01-18 12:43:30 +0200

    create file for adaptive layout for all devices

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
 
-class DashboardView extends StatefulWidget {
-  const DashboardView({
-    super.key,
-  });
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
 
-  @override
-  State<DashboardView> createState() => _DashboardViewState();
-}
-
-class _DashboardViewState extends State<DashboardView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('cpcp'));
+    return Scaffold ();
   }
 }
