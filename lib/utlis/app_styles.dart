274c2343f7969190823a754b09106c0e01021f0b
shroida <shroida012@gmail.com>
2025-01-18 12:51:01 +0200

    create appStyles for app

---
+++ lib/utlis/app_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle styleReg16 = TextStyle(
+      color: Color(0xff064060),
+      fontSize: 16,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400);
+}
